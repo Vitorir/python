e3191c991b5fd234234da729ceaabd949b947d39
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-10-14 19:09:47 -0300

    lp02 2

--- lpa02condicionais/exercicios.py
+++ lpa02condicionais/exercicios.py
@@ -56,3 +56,48 @@ if ano_nascimento > ano_atual or ano_nascimento < 0:
 else:
     idade = ano_atual - ano_nascimento
     print("A idade é:", idade)
+
+# 12
+valor_hora = float(input("digite valor por hora"))
+horas_trabalhadas = int(input("Horas trabalhadas: "))
+
+valor_total = valor_hora * horas_trabalhadas
+ir = valor_total * 0.11
+print(ir)
+inss = valor_total * 0.08
+print(inss)
+sindicato = valor_total * 0.05
+print(sindicato)
+salario_liquido = valor_total - ir - inss - sindicato
+print(salario_liquido)
+
+# lp 07
+'''
+
+7. Depois da liberação do governo para as mensalidades dos planos de saúde, as pessoas começaram a fazer pesquisas para descobrir um bom plano, não muito caro. Um vendedor de um plano de saúde apresentou a tabela a seguir. Faça um programa que entre com o nome e a idade de uma pessoa e imprima o nome e o valor que ela deverá pagar.
+               Idade                                               Valor 
+              Até 10 anos                                     R$30,00 
+             Acima de 10 até 29 anos            R$60,00 
+             Acima de 29 até 45 anos           R$120,00 
+             Acima de 45 até 59 anos           R$150,00 
+             Acima de 59 até 65 anos           R$250,00
+             Maior que 65 anos                       R$400,00
+'''
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+
+if idade <= 10:
+    valor = 30.00
+elif idade <= 29:
+    valor = 60.00
+elif idade <= 45:
+    valor = 120.00
+elif idade <= 59:
+    valor = 150.00
+elif idade <= 65:
+    valor = 250.00
+else:
+    valor = 400.00
+
+print(f"Nome: {nome}")
+print(f"Valor a ser pago: R$ {valor:.2f}")
