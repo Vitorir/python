7671d6935c0feb81f4c1381dc42c264b1d75d672
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-10-25 14:20:03 -0300

    commentsfor

--- jsa03lacoswhile/while.js
+++ jsa03lacoswhile/while.js
@@ -1,32 +1,32 @@
 // 1) Imprima na tela os número de 1 a 10
-// let contador = 1
+let contador = 1
 
-// while (contador <= 10) {    
-//     console.log(contador);
-//     contador++
-// }
+while (contador <= 10) {    
+    console.log(contador);
+    contador++
+}
 
 // 2) Calcule a soma dos números de 1 a 50
-// let soma = 0
-// let contador = 1
-// while (contador <= 50) {
-//     soma += contador
-//     console.log(contador)
-//     contador++
-// }
+let soma = 0
+let contador = 1
+while (contador <= 50) {
+    soma += contador
+    console.log(contador)
+    contador++
+}
 
-// console.log(soma);
+console.log(soma);
 
 // 3) Encontre o fatorial de um determinado número
-// let fat = 1
-// let num = 5
-// let contador = 1
-// while (contador <= num) {
-//     fat = fat * contador
-//     contador++
-// }
+let fat = 1
+let num = 5
+let contador = 1
+while (contador <= num) {
+    fat = fat * contador
+    contador++
+}
 
-// console.log(fat);
+console.log(fat);
 
 // 4) Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o
 // valor seja inválido e continue pedindo até que o usuário informe um valor válido.
@@ -47,16 +47,16 @@ while (userName == senha) {
 }
 
 // 6) Faça um programa que calcule o mostre a média aritmética de N notas
-// let nNotas = parseInt(prompt("Quantidade de notas:"));
-// let cont = 1
-// soma = 0
-// while (cont < nNotas) {
-//     let nota = parseFloat(prompt(`Nota ${cont}:`));
-//     soma += nota
-// }
+let nNotas = parseInt(prompt("Quantidade de notas:"));
+let cont = 1
+soma = 0
+while (cont < nNotas) {
+    let nota = parseFloat(prompt(`Nota ${cont}:`));
+    soma += nota
+}
 
-// media = soma / nNotas
-// console.log(media);
+media = soma / nNotas
+console.log(media);
 
 // 7) Faça um programa que peça para 5 pessoas a sua idade, ao final o programa devera
 // verificar se a média de idade da turma varia entre 0 e 25, 26 e 60 e maior que 60; e então,
