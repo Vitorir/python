fd992ee220bc5ec554f07a752f01eee81037831d
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-11-03 15:17:47 -0300

    615

--- pya03dicsesets/615a03.py
+++ pya03dicsesets/615a03.py
@@ -105,3 +105,26 @@ turma = {
     'Alunos': 30
 }
 
+#Questão 8
+#Dado o dicionário Filme com a seguinte estrutura {"Titulo" : "It a Coisa", "Genero": "Terror", "Duracao": 120, "Cartaz": False}
+#faça um programa que imprima os valores de todas as chaves, uma por #uma, e no final remova a chave "Genero" 
+# e caso ela não exista, mostre #uma mensagem de erro informando que a chave não existe
+
+filme = {"Titulo" : "It a Coisa", "Genero": "Terror", "Duracao": 120, "Cartaz": False}
+for chave in filme.keys():
+    print(chave)
+
+print(filme.pop('ola', 'Não existe genero em filme'))
+
+'''
+#QUESTÃO 9
+#Dado o dicionário Jogo com a seguinte estrutura {"Título" : "League of Legends", "Gênero" : "MOBA" }
+#faça um programa que faça uma copia desse dicionário, e depois atualize a cópia com um novo título (Perfect World)
+# e um novo gênero (MMORPG) e crie também uma novo chave e valor chamado "Lançamento" : 2005
+'''
+jogo = {"Título" : "League of Legends", "Gênero" : "MOBA" }
+jogo2 = jogo.copy()
+jogo2['Título'] = 'Perfect World'
+jogo2['Gênero'] = 'MMORPG'
+jogo2['Lancamento'] = 2005
+print(jogo2)
