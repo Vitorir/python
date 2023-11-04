b17fe42770cf289040447b4a524affa7894d7468
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-11-04 19:12:06 -0300

    lpa04 volley

--- lpa04while/717.py
+++ lpa04while/717.py
@@ -93,6 +93,70 @@ print(f"\nMédias: {media_idade:.2f}, {media_peso}")
 print(f"Mais jovem: {nome_jovem} - {mais_jovem}")
 print(f"Mais pesado: {nome_pesado} - {mais_pesado}")
 
+
+
+
+
+
+# Versao mais complexa
+# Variáveis para o total de países e jogadores
+total_paises = 30
+total_jogadores = total_paises * 12
+
+# Variáveis para o peso total e idade total de todos os jogadores
+peso_total = 0
+idade_total = 0
+
+# Variáveis para armazenar as informações dos atletas mais pesados e mais jovens de cada time
+peso_mais_pesado = 0
+idade_mais_jovem = float('inf')
+
+# Laço para percorrer cada país
+for pais in range(1, total_paises + 1):
+    peso_time = 0
+    idade_time = 0
+
+    # Laço para percorrer cada jogador de um time
+    for jogador in range(1, 13):
+        # Solicitar o peso e a idade do jogador
+        peso = float(input(f"Informe o peso do jogador {jogador} do país {pais}: "))
+        idade = int(input(f"Informe a idade do jogador {jogador} do país {pais}: "))
+
+        # Atualizar o peso total e idade total de todos os jogadores
+        peso_total += peso
+        idade_total += idade
+
+        # Atualizar o peso total e idade total de cada time
+        peso_time += peso
+        idade_time += idade
+
+        # Verificar se o jogador é o mais pesado do time
+        if peso > peso_mais_pesado:
+            peso_mais_pesado = peso
+
+        # Verificar se o jogador é o mais jovem do time
+        if idade < idade_mais_jovem:
+            idade_mais_jovem = idade
+
+    # Calcular o peso médio e idade média de cada time
+    peso_medio_time = peso_time / 12
+    idade_medio_time = idade_time / 12
+
+    # Exibir as informações do time
+    print(f"No time {pais}:")
+    print(f"Peso médio: {peso_medio_time} kg")
+    print(f"Idade média: {idade_medio_time} anos")
+    print()
+
+# Calcular o peso médio e idade média de todos os jogadores
+peso_medio_total = peso_total / total_jogadores
+idade_medio_total = idade_total / total_jogadores
+
+# Exibir as informações de todos os jogadores
+print("No campeonato:")
+print(f"Peso médio de todos os participantes: {peso_medio_total} kg")
+print(f"Idade média de todos os participantes: {idade_medio_total} anos")
+
 '''
 Na Usina de Angra dos Reis, os técnicos analisam a perda de massa de um material radioativo. Sabendo-se que este perde 25% de sua massa a 
 cada 30 segundos, criar um programa em Python que imprima o tempo necessário para que a massa deste material se torne menor que 0,10 grama. 
